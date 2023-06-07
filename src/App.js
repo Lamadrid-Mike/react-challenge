cd680ff37192c34009788f2784d18fffde2a6de7
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-06-06 20:10:14 -0400

    files edited

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
